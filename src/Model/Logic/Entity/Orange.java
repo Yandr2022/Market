cceff18a3765661627c5854307cd8c8586648399
Yandr301187 <sharkievich@gmail.com>
2022-04-27 19:41:06 +0300

    Initial commit

---
+++ src/Model/Logic/Entity/Orange.java
@@ -0,0 +1,34 @@
+package Model.Logic.Entity;
+
+public class Orange {
+    private int diameter;
+    private int vitC;
+    private int cost;
+    public Orange() {
+
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getVitC() {
+        return vitC;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public void setVitC(int vitC) {
+        this.vitC = vitC;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
